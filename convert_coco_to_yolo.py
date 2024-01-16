713d6758e0fba4bfb9030f9ddfa7fbd2a5bbf0eb
ZhenCao <33047738+Ares-cz@users.noreply.github.com>
2024-01-16 20:30:42 +0800

    Update convert_coco_to_yolo.py
    change path

--- convert_coco_to_yolo.py
+++ convert_coco_to_yolo.py
@@ -1,6 +1,6 @@
 from ultralytics.data.converter import convert_coco
 
-path = 'video_79_coco/annotations' #  coco标签的路径
+path = 'path_to_coco_annoatations/annotations' #  coco标签的路径
 
 convert_coco(labels_dir=path,
              save_dir='video_79_coco_converted/',
