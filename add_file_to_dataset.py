3e84189d8dfca4a854c94117da2cf10e2aef389e
ZhenCao <33047738+Ares-cz@users.noreply.github.com>
2024-01-16 20:41:45 +0800

    Update add_file_to_dataset.py
    change path

--- add_file_to_dataset.py
+++ add_file_to_dataset.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+# 将不同数据源的图像和标签，分别复制到一个目录下
+# 以便于后续划分数据集
 def copy_subfolder_contents(root_path, dest_root_path):
     """
     从每个子目录下的 images/ 和 labels/ 中复制文件到指定的目标根目录下的 images/ 和 labels/
@@ -25,7 +27,7 @@ def copy_subfolder_contents(root_path, dest_root_path):
     print(f"文件已从 {root_path} 复制到 {dest_root_path} 的 images/ 和 labels/ 中")
 
 # 示例使用
-source_root_folder = "video_79_yolo_detection"  # 源根目录路径
-destination_root_folder = "Grape_bunch_yolo_dataset"  # 目标根目录路径
+source_root_folder = "path_to_source_root_folder"  # 源根目录路径
+destination_root_folder = "path_to_destination_root_folder"  # 目标根目录路径，即为存放所有数据集的文件夹
 
 copy_subfolder_contents(source_root_folder, destination_root_folder)
